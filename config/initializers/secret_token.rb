23376d25ca905ba121ada8b186f5ad3219708e79
Rustam Gasanov <animeame@gmail.com>
2012-01-20 20:02:18 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OdnoklassnikiOauth::Application.config.secret_token = 'fa45d3baf82b8d6b8bc1c6cb44b4aa98ca157f4d1ef5421642cdfb3ac8994df3ba1afd4b267e090ea601d049f5f8d6f8debcead702b48b1abaf418afad2fab83'
